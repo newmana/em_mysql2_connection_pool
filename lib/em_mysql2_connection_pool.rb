d9118343f4b60d3759b568044f959c4f621e9ef2
Ahmed <ahmed1490@gmail.com>
2014-05-12 12:21:53 +0530

    refresh connections on error

--- lib/em_mysql2_connection_pool.rb
+++ lib/em_mysql2_connection_pool.rb
@@ -2,27 +2,29 @@ require 'mysql2'
 require 'mysql2/em'
 
 class EmMysql2ConnectionPool
-  
+
   class Query
     def initialize(sql, opts, deferrable)
       @sql, @opts, @deferrable = sql, opts, deferrable
     end
-    
+
     def sql(connection)
       @sql.respond_to?(:call) ? @sql.call(connection) : @sql
     end
-    
+
     def execute(connection, &block)
       @busy = true
       @query_text = sql(connection)
+      puts "\n\nEMCLIENT: @query_text : #{@query_text.inspect}\n\n"
       q = connection.query @query_text, @opts
       q.callback{ |result| succeed result, connection.affected_rows, &block }
       q.errback{  |error|  fail error, &block }
       return q
-    rescue StandardError => error
-      fail error, &block
+      rescue StandardError => error
+        puts "\n\nEMCLIENT:ERROR  -> #{error.inspect}\n\n"
+        fail error, &block
     end
-    
+
     def succeed(result, affected_rows, &block)
       @deferrable.succeed result, affected_rows
     rescue StandardError => error
@@ -31,59 +33,68 @@ class EmMysql2ConnectionPool
       @busy and block.call
       @busy = false
     end
-    
+
     def fail(error, &block)
       @deferrable.errback &default_errback unless has_errbacks?
       @deferrable.fail error, @query_text
     ensure
-      @busy and block.call
+      @busy and block.call(true)
       @busy = false
     end
-    
+
     def has_errbacks?
       !@deferrable.errbacks.nil?
     end
-    
+
     def default_errback
       proc{ |error, sql| puts "#{error.class}: '#{error.message}' with query #{sql} in #{error.backtrace.first}" }
     end
-    
+
   end
-  
+
   def initialize(conf)
     @pool_size   = conf[:size] || 10
     @on_error    = conf[:on_error]
     @query_queue = EM::Queue.new
-    start_queue conf
+    @conf = conf
+    start_queue
   end
-  
+
   def query_backlog
     @query_queue.size
   end
-  
+
   def worker
     proc{ |connection|
       @query_queue.pop do |query|
-        query.execute(connection){ worker.call connection }
+        puts "\n!!!EMCLIENT: EXECUTING QUERY #{query_backlog}\n #{query.inspect}\n\n"
+        query.execute(connection) do |refresh_connection|
+          connection = em_connection if refresh_connection
+          worker.call connection
+        end
       end
     }
   end
-  
+
   def start_queue(conf)
     @pool_size.times do
-      worker.call Mysql2::EM::Client.new conf
+      worker.call em_connection
     end
   end
-  
+
+  def em_connection
+    Mysql2::EM::Client.new @conf
+  end
+
   def on_error(&block)
     @on_error = block
   end
-  
+
   def query(sql, opts={})
     deferrable = EM::DefaultDeferrableWithErrbacksAccessor.new
     deferrable.callback{ |result,affected_rows| yield result, affected_rows } if block_given?
     deferrable.errback &@on_error if @on_error
-    
+
     @query_queue.push Query.new(sql, opts, deferrable)
     deferrable
   end
